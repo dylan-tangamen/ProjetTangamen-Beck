131e944b979c096d152151614d07cdb458231033
rbeck20 <remi.beck1@etu.univ-lorraine.fr>
2021-10-12 08:46:36 +0200

    séance 3

--- ProfileService/ProfileService/src/main/java/com/example/ProfileService/ProfileController.java
+++ ProfileService/ProfileService/src/main/java/com/example/ProfileService/ProfileController.java
@@ -9,6 +9,8 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +25,11 @@ public class ProfileController {
 	private final Map<Long, Profile> profiles = new HashMap<>();
 	private final Set<String> emails = new HashSet<>();
 	
+	private Logger logger = LoggerFactory.getLogger(ProfileController.class);
+	
 	@GetMapping("/PS/profiles")
 	public Collection<Profile>profiles(){
+		logger.trace("GET /PS/profiles");
 		return profiles.values();
 	}
 	
@@ -34,6 +39,7 @@ public class ProfileController {
 
 	@PutMapping("/PS/profiles")
 	public Profile profiles_put(@RequestBody @Valid Profile profile) {
+		logger.trace("PUT /PS/profiles");
 		if (emails.contains(profile.getEmail()))
 			throw new EmailInUseException(profile.getEmail());
 		String email = profile.getEmail();
@@ -41,11 +47,13 @@ public class ProfileController {
 		profile.setId(new_id);
 		profiles.put(new_id, profile);
 		emails.add(email);
+		logger.info(String.format("Profile created : [%d] %s.", new_id, profile.getEmail()));
 		return profile;
 	}
 	
 	@DeleteMapping("/PS/profiles/{id}")
 	public void profiles_delete(@PathVariable(value = "id") Long id) {
+		logger.trace("DELETE /PS/profiles/{id}");
 		Profile profile = profiles.get(id);
 		emails.remove(profile.getEmail());
 		profiles.remove(id);
@@ -54,12 +62,14 @@ public class ProfileController {
 	//PUT /PS/profiles/{id}/name
 	@PutMapping("/PS/profiles/{id}/name")
 	public void update_name(@PathVariable(value="id")Long id, @RequestBody @Valid String name) {
+		logger.trace("PUT /PS/profiles/{id}/name");
 		Profile profile = profiles.get(id);
 		profile.setName(name);
 	}
 	
 	@GetMapping("/PS/profiles/{id}/name")
 	public String profile_get_name(@PathVariable(value = "id") Long id){
+		logger.trace("GET /PS/profiles/{id}/name");
 		if (!profiles.containsKey(id))
 			throw new ProfileNotFoundException(id);
 		return profiles.get(id).getName();
@@ -67,6 +77,7 @@ public class ProfileController {
 	
 	@GetMapping("/PS/profiles/{id}/email")
 	public String profile_get_email(@PathVariable(value = "id") Long id){
+		logger.trace("GET /PS/profiles/{id}/email");
 		if (!profiles.containsKey(id))
 			throw new ProfileNotFoundException(id);
 		return profiles.get(id).getEmail();
@@ -74,6 +85,7 @@ public class ProfileController {
 	
 	@GetMapping("/PS/profiles/{id}/description")
 	public String profile_get_description(@PathVariable(value = "id") Long id){
+		logger.trace("GET /PS/profiles/{id}/description");
 		if (!profiles.containsKey(id))
 			throw new ProfileNotFoundException(id);
 		return profiles.get(id).getDescription();
