6121ba40e982f533aa7295f97a6c287df90bf308
Dylan Tangamen <tangamendylan@mail.com>
2021-11-09 09:22:00 +0100

    ajout constructeur

--- AuthService/AuthService/src/main/java/com/example/AuthService/User.java
+++ AuthService/AuthService/src/main/java/com/example/AuthService/User.java
@@ -18,6 +18,11 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public User(long id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
 	
 	 
 	
