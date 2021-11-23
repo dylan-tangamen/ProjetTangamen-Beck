3d87c1faf0bcd699d0aa3d0c98070f6049916c97
Dylan Tangamen <tangamendylan@mail.com>
2021-11-23 11:58:28 +0100

    ajout exceptions

--- AuthService/AuthService/src/main/java/com/example/AuthService/ExceptionHandlingController.java
+++ AuthService/AuthService/src/main/java/com/example/AuthService/ExceptionHandlingController.java
@@ -1,5 +1,43 @@
 package com.example.AuthService;
 
-public class ExceptionHandlingController {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
+public class ExceptionHandlingController {
+	
+	@ResponseBody
+	@ExceptionHandler(TokenNotValidException.class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	String tokenNotValidHandler(TokenNotValidException ex) {
+		return ex.getMessage();
+	}
+	
+	 @ResponseBody
+	 @ExceptionHandler (UserAlreadyExistException.class)
+	 @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+	 String UserAlreadyExistHandler(UserAlreadyExistException ex) {
+		 return ex.getMessage();
+	 }
+	 
+	 @ResponseBody
+	 @ExceptionHandler (IncorrectPasswordException.class)
+	 @ResponseStatus(HttpStatus.UNAUTHORIZED)
+	 String IncorrectPasswordHandler(IncorrectPasswordException ex) {
+		return ex.getMessage(); 
+	 }
+	 
+	 @ResponseBody
+	 @ExceptionHandler (UserNotFoundException.class)
+	 @ResponseStatus(HttpStatus.NOT_FOUND)
+	 String UserNotFoundHandler(UserNotFoundException ex) {
+		 return ex.getMessage();
+	 }
+	 
+
+	
+	
 }
---
+++ AuthService/AuthService/src/main/java/com/example/AuthService/IncorrectPasswordException.java
@@ -0,0 +1,5 @@
+package com.example.AuthService;
+
+public class IncorrectPasswordException {
+
+}
