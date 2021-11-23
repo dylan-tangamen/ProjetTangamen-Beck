5043ba61fbaf7b4f659324f730c1710bba9ee779
rbeck20 <remi.beck1@etu.univ-lorraine.fr>
2021-11-23 11:01:40 +0100

    commit 23/11 11H

--- AuthService/AuthService/src/test/java/com/example/AuthService/AuthServiceApplicationTests.java
+++ AuthService/AuthService/src/test/java/com/example/AuthService/AuthServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.AuthService;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureMockRestServiceServer
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class AuthServiceApplicationTests {
-
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private MockRestServiceServer server;
+	
 	@Test
-	void contextLoads() {
+	public void getUserShouldReturnEmptyArray() throws Exception{
+		this.mockMvc.perform(get("/AS/users/{userId}")).andDo(print()).andExpect(status().isOk()).andExpect(content().json("[]"));
 	}
+
+	
 
 }
